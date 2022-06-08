1f30c3023e9d81dc45d62e51f3605092acba4382
iagochaves <iagovieirachaves@gmail.com>
2022-06-08 10:38:01 -0300

    Added API endpoint for production

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? 'http prod'
+      ? 'https://estaciona-aqui-app.herokuapp.com'
       : 'http://localhost:8080',
 });
 
